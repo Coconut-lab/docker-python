5aed4e6a3d3cc26e66e8f33f4695a6aaa5fbe89e
2coconut <51150721+Coconut-lab@users.noreply.github.com>
2024-05-30 01:23:08 +0900

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,9 +1,9 @@
-#!/bin/bash
+#!/usr/bin/bash
 set -e
 
 # 디렉토리 소유자 변경 (루트 권한으로 실행)
-sudo chown -R python:python /app
-sudo chmod -R 775 /app
+# sudo chown -R python:python /app
+# sudo chmod -R 775 /app
 
 # 파일 권한 설정 (예: discordbot.py)
 chmod +x /app/discordbot.py
